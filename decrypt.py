25d0b2918f28468d8cdb18a7e533f9e9e5e7de3a
CONIGUERO <64615687+CONIGUERO@users.noreply.github.com>
2020-09-09 13:46:02 -0300

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''
-Copyright (C) 2020  The HCDecryptor Authors
+Copyright (C) 2020  HCTools
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
